950fa95c47287302ebb5d50bff2dbd1d7171cd5c
danilo192836 <162758713+danilo192836@users.noreply.github.com>
2024-07-05 21:50:00 -0300

    Colocando o código do ro

---
+++ GenghisKhan.java
@@ -0,0 +1,56 @@
+package GenghisKhan;
+import robocode.*;
+//import java.awt.Color;
+
+// API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
+
+/**
+ * GenghisKhan - a robot by (your name here)
+ */
+public class GenghisKhan extends Robot
+{
+	/**
+	 * run: GenghisKhan's default behavior
+	 */
+	public void run() {
+		// Initialization of the robot should be put here
+
+		// After trying out your robot, try uncommenting the import at the top,
+		// and the next line:
+
+		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+
+		// Robot main loop
+		while(true) {
+			// Replace the next 4 lines with any behavior you would like
+			ahead(200);
+			turnGunRight(360);
+			back(200);
+			turnGunRight(360);
+		}
+	}
+
+	/**
+	 * onScannedRobot: What to do when you see another robot
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// Replace the next line with any behavior you would like
+		fire(300);
+	}
+
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+		// Replace the next line with any behavior you would like
+		back(30);
+	}
+	
+	/**
+	 * onHitWall: What to do when you hit a wall
+	 */
+	public void onHitWall(HitWallEvent e) {
+		// Replace the next line with any behavior you would like
+		back(50);
+	}	
+}
